f6e27612b2adc31aab34f326a4e684eb4a6979a0
sukruthhh <sraje024@uottawa.ca>
2026-06-18 15:10:04 -0400

    adding frontend setup

---
+++ frontend/android/app/src/main/java/com/zelta/stockscreener/MainActivity.java
@@ -0,0 +1,5 @@
+package com.zelta.stockscreener;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zelta.stockscreener',
+  appName: 'Zelta Stock Screener',
+  webDir: 'out'
+};
+
+export default config;
